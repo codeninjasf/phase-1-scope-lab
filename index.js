ee7f759718d543ad58ae07c105a00c03923186c6
arturoalba <ajdalba@gmail.com>
2022-03-27 23:15:18 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Write your code below
+// 
+
+var customerName = "bob"
+const LeastFavoriteCustomer = "John"
+
+function upperCaseCustomerName(){
+    customerName = "BOB"
+
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = "Sonia"
+}
+
+
